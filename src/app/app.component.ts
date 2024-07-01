687584bb6b6e64908a69795a9e53ef7ee36abe5a
2185592 <2185592@corp.trizetto.com>
2024-07-01 11:31:47 +0200

    added ngrx; call an action from the shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { currentUser } from "@mezomon/shared-library-test";
+import { Store } from "@ngrx/store";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'claims';
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(currentUser({userName: ''}));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedLibraryComponent, HeaderComponent, FooterComponent, SidebarComponent } from '@mezomon/shared-library-test'
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 
 @NgModule({
@@ -13,6 +15,12 @@ import { SharedLibraryComponent, HeaderComponent, FooterComponent, SidebarCompon
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: false,
+      connectInZone: true,
+    }),
     SharedLibraryComponent,
     HeaderComponent,
     FooterComponent,
